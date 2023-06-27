b76a28000b81ee9647b17e5c916c3ccf6a8587de
Fernando Costa <fernandocostadev98@gmail.com>
2023-06-27 12:03:25 -0300

    chore: add video repository

---
+++ application/repositories/video_repository.go
@@ -0,0 +1,52 @@
+package repositories
+
+import (
+	"encoder/domain"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
+
+type VideoRepository interface {
+	Insert(v *domain.Video) (*domain.Video, error)
+	Find(id string) (*domain.Video, error)
+}
+
+type VideoRepositoryDB struct {
+	Db *gorm.DB
+}
+
+func NewVideoRepository(db *gorm.DB) *VideoRepositoryDB {
+	return &VideoRepositoryDB{Db: db}
+}
+
+func (repo VideoRepositoryDB) Insert(video *domain.Video) (*domain.Video, error) {
+
+	id, _ := uuid.NewV4()
+
+	if video.ID == "" {
+		video.ID = id.
+			String()
+	}
+
+	err := repo.Db.Create(video).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return video, nil
+}
+
+func (r VideoRepositoryDB) Find(id string) (*domain.Video, error) {
+	var video domain.Video
+
+	r.Db.First(&video, "id = ?", id)
+
+	if video.ID == "" {
+		return nil, fmt.Errorf("video does not exists")
+	}
+
+	return &video, nil
+}
