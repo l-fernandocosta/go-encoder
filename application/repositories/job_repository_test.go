5314004f292c89a94773239692037fd8a5fbe9c3
Fernando Costa <fernandocostadev98@gmail.com>
2023-06-27 12:53:49 -0300

    chore(test): add test Update method at `job_repository_test.go`

--- application/repositories/job_repository_test.go
+++ application/repositories/job_repository_test.go
@@ -4,6 +4,7 @@ import (
 	"encoder/application/repositories"
 	"encoder/domain"
 	"encoder/framework/database"
+	"fmt"
 	"testing"
 	"time"
 
@@ -38,4 +39,38 @@ func TestJobRepository_INSERT(t *testing.T) {
 	require.Equal(t, j.ID, job.ID)
 	require.Equal(t, j.Video.ID, video.ID)
 
+}
+
+func TestJobRepositoryDBUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	id, _ := uuid.NewV4()
+	video.ID = id.String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDB{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("outputh_path", "PENDING", video)
+
+	require.Nil(t, err)
+
+	jobRepo := repositories.JobRepositoryDB{Db: db}
+	jobRepo.Insert(job)
+
+	job.Status = "Complete"
+
+	fmt.Println(*job)
+	jobRepo.Update(job)
+	j, err := jobRepo.Find(job.ID)
+	fmt.Println(*job)
+	fmt.Println(*j)
+
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.ID, job.ID)
+	require.Equal(t, j.Status, job.Status)
 }
