a5bf2b50360c677d167e6217c38670b92d1448e5
Fernando Costa <fernandocostadev98@gmail.com>
2023-06-27 10:44:07 -0300

    chore(test): add `video` and `job` tests

---
+++ domain/domain_test/job_test.go
@@ -0,0 +1,25 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewJob(t *testing.T) {
+	uid, _ := uuid.NewV4()
+
+	video := domain.NewVideo()
+	video.ID = uid.String()
+	video.CreatedAt = time.Now()
+	video.FilePath = "path"
+	video.ResourceID = "a"
+
+	job, err := domain.NewJob("OUTPUT", "CONVERTED", video)
+
+	require.NotNil(t, job)
+	require.Nil(t, err)
+}
---
+++ domain/domain_test/video_test.go
@@ -0,0 +1,44 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIsNotAValidUUID(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "INVALID_UUID"
+	video.ResourceID = "uuid"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	uid, _ := uuid.NewV4()
+	video.ID = uid.String()
+	video.ResourceID = "uuid"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Nil(t, err)
+}
